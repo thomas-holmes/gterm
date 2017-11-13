a8a455e7a1f2f6abd0f1d2282d0d5cab71a56e8e
Thomas Holmes <shadox@gmail.com>
2017-11-13 00:48:46 -0500

    Move tile into its own file

--- gterm/example/muncher/game/world.go
+++ gterm/example/muncher/game/world.go
@@ -7,28 +7,6 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-type Tile struct {
-	Dirty           bool
-	BackgroundColor sdl.Color
-	BackgroundGlyph string
-	Wall            bool
-}
-
-func NewTile() Tile {
-	return Tile{
-		Dirty:           true,
-		BackgroundColor: sdl.Color{R: 192, G: 192, B: 192, A: 255},
-		BackgroundGlyph: ".",
-	}
-}
-
-func (tile Tile) RenderBackground(col int, row int, window *gterm.Window) {
-	err := window.AddToCell(col, row, tile.BackgroundGlyph, tile.BackgroundColor)
-	if err != nil {
-		log.Println("Failed to render background", err)
-	}
-}
-
 type Position struct {
 	XPos int
 	YPos int
